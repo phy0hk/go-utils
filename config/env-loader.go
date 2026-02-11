894985d9513c33775c7835d2d3ccf3ddce4262a3
phy0hk <phyoheinko.phk1@gmail.com>
2026-02-11 15:35:28 +0630

    Create EnvLoader

--- config/env-loader.go
+++ config/env-loader.go
@@ -1,1 +1,18 @@
 package config
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file: " + err.Error())
+		return
+	}
+	fmt.Println("Loaded .env successfully")
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/phy0hk/go-utils
 
 go 1.25.6
+
+require github.com/joho/godotenv v1.5.1
